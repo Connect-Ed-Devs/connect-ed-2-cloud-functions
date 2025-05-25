674463c03f5858308a030b8a215a64cdd56b1dbc
UtlimateLade <demilade_olawunmi@icloud.com>
2025-05-25 10:41:50 -0400

    deleted

--- functions/src/services/firestoreService.js
+++
@@ -1,9 +0,0 @@
-// javascript
-import admin from 'firebase-admin';
-
-// Initialize Firebase Admin SDK
-export async function addGame(game) {
-  const db = admin.firestore();
-  const docRef = await db.collection('games').add(game.toMap());
-  return docRef;
-}
